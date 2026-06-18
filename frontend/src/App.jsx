8122a2ea9980743377a28497a8d5a954f49c4884
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-06-18 03:44:53 -0700

    added the router for page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// dependency imports
+import { Suspense, useState } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import './index.css'
+
+// assest imports
 import placeholder from './assets/placeholder.png'
 
+// page imports
+import Rsvp from './Rsvp'
+import TravelnStay from './TravelnStay'
+import Wedding from './Wedding'
+
+// component imports
 import Navbar from './Navbar'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+//helper component to make sure the Navbar is always visible
+function Layout({ Page }) {
   return (
     <>
-      <header>TITLE</header>
-      <Navbar/>
+      <Navbar />
+      <Page />
     </>
-  )
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout Page={Wedding} />} />
+        <Route path="/travelnstay" element={<Layout Page={TravelnStay} />} />
+        <Route path="/rsvp" element={<Layout Page={Rsvp} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
