810e3a6a8e327cdcb611867395bce1afbc1046a9
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-06-20 01:16:45 -0700

    updated Navibar

---
+++ frontend/src/Navibar.jsx
@@ -0,0 +1,21 @@
+import placeholder from './assets/placeholder.png'
+
+import { Link } from 'react-router-dom';
+
+function Navibar() {
+  return (
+    <nav className='Navibar'>
+      <Link  to="/">
+        <img src={placeholder} />
+      </Link>
+
+      <ul className="navibar-links">
+        <li><Link to="/">Wedding</Link></li>
+        <li><Link to="/travelnstay">Travel & Stay</Link></li>
+        <li><Link to="/rsvp">RSVP</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navibar;
