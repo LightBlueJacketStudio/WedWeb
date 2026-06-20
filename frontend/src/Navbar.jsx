3bf36b8d7372052e39e239784c1469fb0a59cad4
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-06-20 01:00:15 -0700

    update layout for Navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -6,16 +6,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <span className='Navbar'>
-      <img src={placeholder} ></img>
-      <nav>
-        <ul className="navbar-links">
-          <li><Link to="/">Wedding</Link></li>
-          <li><Link to="/travelnstay">Travel & Stay</Link></li>
-          <li><Link to="/rsvp">RSVP</Link></li>
-        </ul>
-      </nav>
-    </span>
+    <nav className='Navbar'>
+      <Link  to="/">
+        <img src={placeholder} />
+      </Link>
+
+      <ul className="navbar-links">
+        <li><Link to="/">Wedding</Link></li>
+        <li><Link to="/travelnstay">Travel & Stay</Link></li>
+        <li><Link to="/rsvp">RSVP</Link></li>
+      </ul>
+    </nav>
   );
 }
 
