4048f28a2f20e252858af51c89667ce3582e8961
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-06-27 18:26:56 -0500

    rearrange the navibar items so flexbox works better

--- frontend/src/Navibar.jsx
+++ frontend/src/Navibar.jsx
@@ -5,11 +5,8 @@ import { Link } from 'react-router-dom';
 function Navibar() {
   return (
     <nav className='Navibar'>
-      <Link  to="/">
-        <img src={placeholder} />
-      </Link>
-
       <ul className="navibar-links">
+        <li><Link to="/"><img src={placeholder}/></Link></li>
         <li><Link to="/">Wedding</Link></li>
         <li><Link to="/travelnstay">Travel & Stay</Link></li>
         <li><Link to="/rsvp">RSVP</Link></li>
