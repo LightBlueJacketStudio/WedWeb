9598cbcfb63bba3d8558114baa24cf6c673d2713
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-06-26 21:58:06 -0500

    added comments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// entry point for our react app,
+// usually we shouldn't make changes in here
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
