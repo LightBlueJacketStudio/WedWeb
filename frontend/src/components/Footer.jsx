7bd64b1fd95fb38cacf0a904bc186bc09828dda0
LightBlueJacketStudio <lightbluejacketstudio@gmail.com>
2026-07-20 01:03:02 -0500

    removed bottom links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,12 +9,6 @@ function Footer() {
           <span className="material-symbols-outlined">pets</span>
         </div>
 
-        <div className="footer-links">
-          <a href="#journey">Our Story</a>
-          <a href="#contact">Contact Us</a>
-          <a href="#privacy">Privacy</a>
-        </div>
-
         <p>2027 • Made with love &amp; paw prints</p>
       </div>
     </footer>
